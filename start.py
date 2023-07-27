a2c0ee826d384407999d6140e09f5caeb72c09bf
Luka Š <lukasenk77@gmail.com>
2023-07-27 21:46:49 +0200

    ekstrakcija teksta iz PDF datoteke

--- start.py
+++ start.py
@@ -1,1 +1,9 @@
-print ("Hello world!")
+from PyPDF2 import PdfReader
+from pdfminer.high_level import extract_text
+
+
+# pdf = PdfReader(r"C:\Users\luka_\Desktop\Python\scriptK\testni.pdf")
+# strani = pdf.pages[0]
+text = extract_text(r"C:\Users\luka_\Desktop\Python\scriptK\thermo-tek_bd_050_wbs.pdf")
+print(repr(text))
+#print(strani.extract_text())
